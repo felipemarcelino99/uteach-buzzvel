4e4f44457616188d2eb32672fc6df4b803e6b53e
Felipe Marcelino <felipe99marcelino@hotmail.com>
2025-05-07 00:15:26 -0300

    feat: improve seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Head from "next/head";
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -24,6 +25,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>Uteach | Teach students worldwide</title>
+        <meta
+          name="description"
+          content="Amet nunc diam orci duis ut sit diam arcu, nec. Eleifend proin massa tincidunt viverra lectus pulvinar. Nunc ipsum est pellentesque turpis ultricies."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="keywords"
+          content="uTeach, students, courses, online, tecnology, educational platform, education"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="uTeach | Educational platform" />
+        <meta
+          property="og:description"
+          content="Amet nunc diam orci duis ut sit diam arcu, nec. Eleifend proin massa tincidunt viverra lectus pulvinar. Nunc ipsum est pellentesque turpis ultricies."
+        />
+        <meta property="og:image" content="https://uteach.com/og-image.png" />
+        <meta property="og:url" content="https://uteach.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="uTeach |  Educational platform" />
+        <meta
+          name="twitter:description"
+          content="Amet nunc diam orci duis ut sit diam arcu, nec. Eleifend proin massa tincidunt viverra lectus pulvinar. Nunc ipsum est pellentesque turpis ultricies."
+        />
+        <meta name="twitter:image" content="https://uteach.com/og-image.png" />
+      </Head>
+
       <body
         className={`${roboto.variable} antialiased font-[family-name:var(--font-roboto)] overflow-x-hidden`}
       >
